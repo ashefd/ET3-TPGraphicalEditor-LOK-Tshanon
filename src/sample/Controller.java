7e2ca72255d1c3edbd5ad77b78f7625869202ebd
tshanon <tshanon.lok@gmail.com>
2020-05-29 10:46:28 +0200

    commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,29 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    javafx.scene.control.RadioButton mySelect;
+
+    @FXML
+    javafx.scene.control.RadioButton myEllipse;
+
+    @FXML
+    javafx.scene.control.RadioButton myRectangle;
+
+    @FXML
+    javafx.scene.control.RadioButton myLine;
+
+    @FXML
+    javafx.scene.control.ColorPicker myColorPicker;
+
+    @FXML
+    javafx.scene.control.Button myDelete;
+
+    @FXML
+    javafx.scene.control.Button myClone;
 
 
 }
